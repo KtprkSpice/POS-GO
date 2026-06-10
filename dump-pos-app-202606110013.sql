c4aa8db2eeee6edbe172c29b7aaa3a359749e1f4
KtprkSpice <firhansmdzky@gmail.com>
2026-06-11 00:14:05 +0700

    add latest database settings

---
+++ dump-pos-app-202606110013.sql
@@ -0,0 +1,164 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pos-app
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cashiers`
+--
+
+DROP TABLE IF EXISTS `cashiers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cashiers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `phone` varchar(100) NOT NULL,
+  `wallet_address` varchar(255) NOT NULL,
+  `home_address` text,
+  `user_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `wallet_address` (`wallet_address`),
+  KEY `FK_cashiers_users` (`user_id`),
+  CONSTRAINT `FK_cashiers_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cashiers`
+--
+
+LOCK TABLES `cashiers` WRITE;
+/*!40000 ALTER TABLE `cashiers` DISABLE KEYS */;
+INSERT INTO `cashiers` VALUES (1,'086321xxx','addrrr','Ciputat',3,'2026-06-09 08:09:26','2026-06-09 08:09:26',NULL);
+/*!40000 ALTER TABLE `cashiers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `owners`
+--
+
+DROP TABLE IF EXISTS `owners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `owners` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `phone` varchar(100) NOT NULL,
+  `wallet_address` varchar(100) NOT NULL,
+  `user_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `wallet_address` (`wallet_address`),
+  KEY `FK__users` (`user_id`),
+  CONSTRAINT `FK__users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owners`
+--
+
+LOCK TABLES `owners` WRITE;
+/*!40000 ALTER TABLE `owners` DISABLE KEYS */;
+INSERT INTO `owners` VALUES (1,'08236xxx','werrrsa231',2,'2026-06-09 08:09:50','2026-06-09 08:09:50',NULL);
+/*!40000 ALTER TABLE `owners` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `suppliers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `phone` varchar(100) NOT NULL,
+  `wallet_address` varchar(100) NOT NULL,
+  `farm_name` varchar(100) NOT NULL,
+  `farm_address` text NOT NULL,
+  `user_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `wallet_address` (`wallet_address`),
+  KEY `FK_suppliers_users` (`user_id`),
+  CONSTRAINT `FK_suppliers_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+LOCK TABLES `suppliers` WRITE;
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+INSERT INTO `suppliers` VALUES (1,'08236xxxx','gasdxxx231','Metani Jaya','Aceh',1,'2026-06-09 08:10:25','2026-06-09 08:10:26',NULL),(2,'0312361723123','0x92313xsd2332','Mandiri','Sumatra',4,'2026-06-09 09:15:39','2026-06-09 09:15:39',NULL),(4,'085814079866','0x92313xsd2221','Sutri Park','Papua',6,'2026-06-09 09:21:29','2026-06-09 09:21:29',NULL),(5,'0732184123','3x7328Ydx3w','Ciputat Beans Subur','Ciputat',7,'2026-06-09 09:24:20','2026-06-10 17:10:16',NULL),(6,'03712841321','0x92313xsd2321','Sutri Subur Sekali','Jawa',9,'2026-06-10 16:38:50','2026-06-10 16:38:50',NULL);
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` enum('supplier','cashier','owner') NOT NULL DEFAULT 'supplier',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'supplier','supplier@mail.com','password','supplier','2026-06-09 08:08:22','2026-06-09 08:08:23',NULL),(2,'owner','owner@mail.com','password','owner','2026-06-09 08:08:40','2026-06-09 08:08:41',NULL),(3,'cashier','cashier@mail.com','password','cashier','2026-06-09 08:08:58','2026-06-09 08:08:58',NULL),(4,'Trio Masdi Cahyo','triomasdi@mail.com','$2a$10$3RS.uAwk/e8KfeN5EAWS1uZ7oW3un5Q5LhRMTxd9tyYx1d84IHUdu','supplier','2026-06-09 09:15:39','2026-06-09 09:15:39',NULL),(6,'Sutri','sutri@mail.com','$2a$10$L/IWZlHBo2a/Y3KRmZ/4GelXtpIqLAWy5tsHoT7J4uGTtdmdj9qKW','supplier','2026-06-09 09:21:29','2026-06-09 09:21:29',NULL),(7,'Dawnins','daw@mail.com','$2a$10$tucs/rXYG9SX1kxeGXcPqOPu9mxU582LlkCUdQwteO1xUwXQznY2e','supplier','2026-06-09 09:24:20','2026-06-10 17:10:16',NULL),(9,'Sutri Marpriadi','sutimarp12@mail.com','$2a$10$yWYQfzYYdQq3e3rrzUZ3L.7QGaBH/TbiXiRkl5coiffUz22XB8sR6','supplier','2026-06-10 16:38:50','2026-06-10 16:38:50',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'pos-app'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-11  0:13:42
